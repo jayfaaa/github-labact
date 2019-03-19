62083c257c93b67ca08bb20a6d1e6fb29e0f1456
Arch Jay Jabla <bliltinked@gmail.com>
2019-03-19 15:53:46 +0800

    Jabla Commit

---
+++ application/views/home.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title> Lab activity Git and Git HUB </title>
+<link href="assets/css/mystyle.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<h1> THIS IS MY LAB ACTIVITY - JABLA </h1>
+	<script src="<?php echo "assets/js/myjs.js"; ?>" ></script>
+</body>
+</html>
---
+++ assets/js/myjs.js
@@ -0,0 +1,1 @@
+console.log("Jabla Activity")
